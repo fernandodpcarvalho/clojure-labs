f204078d10b597abbcea25eecfb593c3a4081d13
Fernando <fernando.carvalho@paygo.com.br>
2021-01-14 13:58:37 -0300

    todo ok with swagger json

--- pedestal/todo/src/todo/service.clj
+++ pedestal/todo/src/todo/service.clj
@@ -7,48 +7,47 @@
             [schema.core :as s]
             [todo.interceptors :refer :all]))
 
-;(def no-csp
-;  {:name  ::no-csp
-;   :leave (fn [ctx]
-;            (assoc-in ctx [:response :headers "Content-Security-Policy"] ""))})
+(def no-csp
+  {:name  ::no-csp
+   :leave (fn [ctx]
+            (assoc-in ctx [:response :headers "Content-Security-Policy"] ""))})
 
-;(defn build-routes [doc routes]
-;  (-> routes
-;    route/expand-routes
-;    api.routes/replace-splat-parameters
-;    (api.routes/update-handler-swagger
-;      (api.routes/comp->> api.routes/default-operation-ids
-;        api.routes/default-empty-parameters))
-;    (sw.doc/with-swagger (merge {:basePath ""} doc))))
-;
-;(def swagger-doc
-;  {:info {:title       "Todo list API"
-;          :description "Register and consult you todo list"
-;          :version     "1.0"}
-;   :tags [{:name        "Todo list"
-;           :description "Register and consult you todo list"}]})
+(defn build-routes [doc routes]
+  (-> routes
+    route/expand-routes
+    api.routes/replace-splat-parameters
+    (api.routes/update-handler-swagger
+      (api.routes/comp->> api.routes/default-operation-ids
+        api.routes/default-empty-parameters))
+    (sw.doc/with-swagger (merge {:basePath ""} doc))))
+
+(def swagger-doc
+  {:info {:title       "Todo list API"
+          :description "Register and consult you todo list"
+          :version     "1.0"}
+   :tags [{:name        "Todo list"
+           :description "Register and consult you todo list"}]})
 
 (def routes
-  (route/expand-routes
-    #{["/todo" :post [db-interceptor list-create]]
-      ["/todo" :get [db-interceptor all-lists-view]]
-      ["/todo/:list-id" :get [entity-render db-interceptor list-view]]
-      ["/todo/:list-id" :post [entity-render list-item-view db-interceptor list-item-create]]
-      ["/todo/:list-id/:item-id" :get [entity-render list-item-view db-interceptor]]
-      ["/todo/:list-id/:item-id" :put echo :route-name :list-item-update]
-      ["/todo/:list-id/:item-id" :delete echo :route-name :list-item-delete]
-      ;["/swagger.json" :get [(api/negotiate-response) (api/body-params) api/common-body (api/coerce-request) (api/validate-response) api/swagger-json]]
-      ;["/*resource" :get [(api/negotiate-response) (api/body-params) api/common-body (api/coerce-request) (api/validate-response) no-csp api/swagger-ui]]
-      }))
+  #{["/todo" :post [db-interceptor (api/doc {:tags ["transactions"]}) list-create]]
+    ["/todo" :get [db-interceptor all-lists-view]]
+    ["/todo/:list-id" :get [entity-render db-interceptor list-view]]
+    ["/todo/:list-id" :post [entity-render list-item-view db-interceptor list-item-create]]
+    ["/todo/:list-id/:item-id" :get [entity-render list-item-view db-interceptor]]
+    ["/todo/:list-id/:item-id" :put echo :route-name :list-item-update]
+    ["/todo/:list-id/:item-id" :delete echo :route-name :list-item-delete]
+    ["/swagger.json" :get [(api/negotiate-response) (api/body-params) api/common-body (api/coerce-request) (api/validate-response) api/swagger-json]]
+    ;["/*resource" :get [no-csp api/swagger-ui]]
+    })
 
-;(defn init-routes []
-;  (s/with-fn-validation
-;    (build-routes swagger-doc routes)))
+(defn init-routes []
+  (s/with-fn-validation
+    (build-routes swagger-doc routes)))
 
 (def service
   {:env                     :prod
-   ;::http/routes            (init-routes)
-   ::http/routes            routes
+   ::http/routes            (init-routes)
+   ;::http/routes            routes
    ::http/resource-path     "/public"
    ::http/type              :jetty
    ::http/port              8080

01464db25380c6506d3e1daec9474b1aa0ba8a1d
Fernando <fernando.carvalho@paygo.com.br>
2021-01-14 14:39:47 -0300

    todo ok with swagger all valid routes

--- pedestal/todo/src/todo/service.clj
+++ pedestal/todo/src/todo/service.clj
@@ -1,9 +1,6 @@
 (ns todo.service
   (:require [io.pedestal.http :as http]
-            [io.pedestal.http.route :as route]
-            [pedestal-api.routes :as api.routes]
             [pedestal-api.core :as api]
-            [route-swagger.doc :as sw.doc]
             [schema.core :as s]
             [todo.interceptors :refer :all]))
 
@@ -11,15 +8,6 @@
   {:name  ::no-csp
    :leave (fn [ctx]
             (assoc-in ctx [:response :headers "Content-Security-Policy"] ""))})
-
-(defn build-routes [doc routes]
-  (-> routes
-    route/expand-routes
-    api.routes/replace-splat-parameters
-    (api.routes/update-handler-swagger
-      (api.routes/comp->> api.routes/default-operation-ids
-        api.routes/default-empty-parameters))
-    (sw.doc/with-swagger (merge {:basePath ""} doc))))
 
 (def swagger-doc
   {:info {:title       "Todo list API"
@@ -29,10 +17,10 @@
            :description "Register and consult you todo list"}]})
 
 (def routes
-  #{["/todo" :post [db-interceptor (api/doc {:tags ["transactions"]}) list-create]]
-    ["/todo" :get [db-interceptor all-lists-view]]
-    ["/todo/:list-id" :get [entity-render db-interceptor list-view]]
-    ["/todo/:list-id" :post [entity-render list-item-view db-interceptor list-item-create]]
+  #{["/todo" :post [db-interceptor (api/doc {:tags ["create-list"]}) create-list]]
+    ["/todo" :get [db-interceptor (api/doc {:tags ["get-all-lists"]}) get-all-lists]]
+    ["/todo/:list-id" :get [entity-render db-interceptor (api/doc {:tags ["get-list"]}) get-list]]
+    ["/todo/:list-id" :post [entity-render list-item-view db-interceptor (api/doc {:tags ["create-list-item"]}) create-list-item]]
     ["/todo/:list-id/:item-id" :get [entity-render list-item-view db-interceptor]]
     ["/todo/:list-id/:item-id" :put echo :route-name :list-item-update]
     ["/todo/:list-id/:item-id" :delete echo :route-name :list-item-delete]
@@ -40,14 +28,12 @@
     ;["/*resource" :get [no-csp api/swagger-ui]]
     })
 
-(defn init-routes []
-  (s/with-fn-validation
-    (build-routes swagger-doc routes)))
+(s/with-fn-validation
+  (api/defroutes routes swagger-doc routes))
 
 (def service
   {:env                     :prod
-   ::http/routes            (init-routes)
-   ;::http/routes            routes
+   ::http/routes            routes
    ::http/resource-path     "/public"
    ::http/type              :jetty
    ::http/port              8080
